d0a833cb1995b4e660b6b01026ffa9e2b66a05eb
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-09-23 00:21:11 +0800

    Create picgo.R

---
+++ R/picgo.R
@@ -0,0 +1,25 @@
+picgo_use = function() {
+
+}
+
+picgo_config = function() {
+
+}
+
+picgo_upload = function() {
+
+}
+
+picgo_add = function() {
+
+}
+
+picgo_rm = function() {
+
+}
+
+picgo_update = function() {
+
+}
+
+
